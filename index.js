6f9ff3c9d918dd4e5cc3d17b8f6494a59b7840b5
adamsJS23 <adamaguebre@yahoo.fr>
2023-08-06 18:10:53 +0200

    set up code snippet for learning react

---
+++ index.js
@@ -0,0 +1,21 @@
+function index() {
+  return <div></div>;
+}
+
+export default index;
+
+console.log();
+
+import styled from 'styled-components'
+
+const Styledindex = styled.``
+
+function index() {
+  return (
+    <Styledindex>
+      index
+    </Styledindex>
+  )
+}
+
+export default index
